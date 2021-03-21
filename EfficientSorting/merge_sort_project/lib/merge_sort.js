57945c807baad50cbef959cc10df95a340e0be1d
LidiaBaciu <lidia.baciu97@gmail.com>
2021-03-21 15:43:42 +0200

    Implemented merge sort

---
+++ EfficientSorting/merge_sort_project/lib/merge_sort.js
@@ -0,0 +1,37 @@
+function merge(array1, array2) {
+	let merged = []
+
+	while (array1.length || array2.length) {
+		let element1 = array1.length ? array1[0] : Infinity
+		let element2 = array2.length ? array2[0] : Infinity
+
+		let next
+		if (element1 < element2) {
+			//remove and return the first element
+			next = array1.shift()
+		} else {
+			next = array2.shift()
+		}
+
+		merged.push(next)
+	}
+	return merged
+}
+
+function mergeSort(array) {
+	if (array.length <= 1) return array
+
+	let midIndex = Math.floor(array.length / 2)
+	let leftHalf = array.slice(0, midIndex)
+	let rightHalf = array.slice(midIndex)
+
+	let sortedLeft = mergeSort(leftHalf)
+	let sortedRight = mergeSort(rightHalf)
+
+	return merge(sortedLeft, sortedRight)
+}
+
+module.exports = {
+	merge,
+	mergeSort,
+}
